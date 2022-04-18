ef77760045e5fca1291919ebca22740dfbdcd9d7
Jiyan Tunc <85908648+JiyanT@users.noreply.github.com>
2022-04-18 13:30:36 -0700

    Array

---
+++ src/Day29/EvenOrOdd.java
@@ -0,0 +1,43 @@
+package Day29;
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        int[] nums={4,1,3,12,5,20,13};
+
+        int even=0;
+        int odd= 0;
+
+        String evenNums="";
+        String oddNums="";
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 2 == 0) {
+                even++;
+                evenNums += nums[i] + " ";
+            } else {
+                odd++;
+                oddNums += nums[i] + " ";
+            }
+        }
+            System.out.println("even counter: " + even);
+            System.out.println("Even numbers: " + evenNums);
+
+            System.out.println("Odd counter: " + odd);
+            System.out.println("Even numbers: " + evenNums);
+
+
+            for (int number: nums){
+                if (number % 2 == 0){
+                    even++;
+                    evenNums += number+ " ";
+                } else {
+                    odd++;
+                    oddNums += number + " ";
+                }
+            }
+
+
+    }
+}
