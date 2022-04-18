f040a1006b994a760673f318ec0e06d880d5b44e
Jiyan Tunc <85908648+JiyanT@users.noreply.github.com>
2022-04-18 15:56:31 -0700

    Array

---
+++ src/Day29/Initials.java
@@ -0,0 +1,17 @@
+package Day29;
+
+public class Initials {
+
+    public static void main(String[] args) {
+
+        String [] names={"James Bond", "Eve Rell", "Anna Johnson"};
+
+        for (int i = 0; i < names.length; i++) {
+
+            String fullName= names[i].trim();
+            String lastName = fullName.substring(fullName.indexOf(" ")).trim() ;
+            System.out.println( "" + fullName.charAt(0) + lastName.charAt(0) );
+
+        }
+    }
+}
